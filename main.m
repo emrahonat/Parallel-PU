67744b6298eb4d65c6443249ced4076f19fab862
emrah onat <eonat87@yahoo.com>
2025-11-16 00:21:55 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,13 @@
+%% Parallelization of Path Following Based Phase Unwrapping Algorithms
+
+% 
+% 0 - Input Images (Interferograms)
+% 1 - Despekling Algorithms
+% 2 - Map Generation
+% 3 - Phase Unwrapping Algorithms
+%
+% Dr. Emrah Onat
+% 16.11.2025
+% 
+
+%% Main Code 
